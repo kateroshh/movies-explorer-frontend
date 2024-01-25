1f381a152accb01ee3549e95e63c60ec66293677
Kate Roshchupkina <katerina.ermakov@mail.ru>
2024-01-25 15:05:04 +0300

    Fix errors: NotFound go back, stage 1

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,22 @@
 import './NotFound.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+  const goBack = (e) => {
+    e.preventDefault();
+    navigate(-1);
+  };
+
   return (
     <div className='not-found'>
       <div className='not-found__content'>
         <h1 className='not-found__title'>404</h1>
         <p className='not-found__text'>Страница не найдена</p>
       </div>
-      <a className='not-found__link' href='#'>
+      <Link className='not-found__link' to={'..'} onClick={goBack}>
         Назад
-      </a>
+      </Link>
     </div>
   );
 }
